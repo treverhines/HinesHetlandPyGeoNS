beab9f59b989fcaadff87d1ae25c4cf067af77a0
Trever Hines <treverhines@gmail.com>
2016-09-02 08:06:46 -0400

    added demo3

--- examples/demo3.py
+++ examples/demo3.py
@@ -1,72 +1,95 @@
 #!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import rbf.fd
-import rbf.halton
-import sympy
-import scipy.linalg
-import scipy.signal
-np.random.seed(6)
-H = rbf.halton.Halton(1)
+import rbf.nodes
+import rbf.domain
+import scipy.sparse
+np.random.seed(4)
+
+# setup axes
+fig = plt.figure(figsize=(10,8))
+gs = gridspec.GridSpec(2,2)
+gs.update(left=0.1,right=0.95,top=0.95,bottom=0.1,hspace=0.2,wspace=0.2)
+ax1 = plt.subplot(gs[0,0])
+ax2 = plt.subplot(gs[0,1])
+ax3 = plt.subplot(gs[1,0])
+ax4 = plt.subplot(gs[1,1])
+ax1.set_aspect('equal')
+ax2.set_aspect('equal')
+ax3.set_aspect('equal')
+ax4.set_aspect('equal')
+ax1.set_xlim((-11,11))
+ax2.set_xlim((-11,11))
+ax3.set_xlim((-11,11))
+ax4.set_xlim((-11,11))
+ax1.set_ylim((-11,11))
+ax2.set_ylim((-11,11))
+ax3.set_ylim((-11,11))
+ax4.set_ylim((-11,11))
+
+# make nodes
+P = 10000
+diff = 2
+vert,smp = rbf.domain.circle(5)
+vert *= 10.0
+
+fix_nodes = np.array([[0.0,0.0],
+                      [-10.0,0.0]])
+nodes,sid = rbf.nodes.make_nodes(P-2,vert,smp,itr=100,delta=0.1,neighbors=10,
+                                 fix_nodes=fix_nodes)
+nodes = np.vstack((fix_nodes,nodes))
 
 
-def extend_data(time,data,var):
-  R = len(time)
-  print(R)
-  new_time = max_time*H(R)[:,0]
-  new_data = np.zeros(R)
-  new_var = np.inf*np.ones(R)
-  time = np.concatenate((time,new_time))
-  data = np.concatenate((data,new_data))
-  var = np.concatenate((var,new_var))
-  sort_idx = np.argsort(time)
-  time  = time[sort_idx]
-  data = data[sort_idx]
-  var = var[sort_idx]
-  return time,data,var
-  
-N = 2
-P = 10
-cutoff = 0.5
-signal_freq = 1.0
-min_time = 0.0
-max_time = 5.0
-var = 1.0*np.ones(P)
-time = np.sort(max_time*H(P)[:,0])
-  
-signal = 1*np.sin(2*np.pi*time*signal_freq)
-data = signal + np.random.normal(0.0,np.sqrt(var))
+# make synthetic data
+u = np.random.normal(0.0,1.0,P)
 
+D = rbf.fd.diff_matrix(nodes,[(diff,0),(0,diff)])
+I = scipy.sparse.eye(P)
+# smooth
+sigma = 1.0
+cutoff = 1.0/6.0
+penalty = (2*np.pi*cutoff)**(2*diff)*sigma**2
 
-for i in range(6):
-  data = np.nan_to_num(data)
-  # compute differentiation matrix
-  
-  D = rbf.fd.poly_diff_matrix(time[:,None],(N,)).toarray()
-  var_bar = 1.0/np.mean(1.0/var)
-  lamb_square = (2*np.pi*cutoff)**(2*N)*var_bar
-  Cobs_inv = np.diag(1.0/var)
-  Cprior_inv = 1.0/lamb_square*D.T.dot(D)
-  Cpost = np.linalg.inv(Cobs_inv + Cprior_inv)
-  upost = Cpost.dot(Cobs_inv).dot(data)
-  stdpost = np.sqrt(np.diag(Cpost))
+us = scipy.sparse.linalg.spsolve(I + (1.0/penalty)*D.T.dot(D),u)
+cov = np.linalg.inv((I + (1.0/penalty)*D.T.dot(D)).toarray())
+sigma = np.sqrt(np.diag(cov))
+corr = (cov/sigma[None,:])/sigma[:,None]
 
-  data[np.isinf(var)] = np.nan
-  # plot one of the data and filter realizations 
-  fig1,ax1 = plt.subplots(figsize=(6,5))
-  ax1.set_xlabel(r'time [yr]')
-  ax1.set_ylabel(r'displacement [mm]')
-  ax1.errorbar(time,data,np.sqrt(var),fmt='k.',label=r'$u_\mathrm{obs}$',capsize=0,zorder=2)
-  ax1.plot(time,upost,'b-',lw=2,label=r'$u_\mathrm{post}$',zorder=3)
-  #ax1.plot(time,signal,'r-',lw=2,label=r'$u_\mathrm{true}$',zorder=1)
-  ax1.set_ylim((-5,7))
-  ax1.grid()
-  ax1.legend(frameon=False)
-  ax1.fill_between(time,upost+stdpost,upost-stdpost,color='b',alpha=0.3,edgecolor='none')
-  fig1.tight_layout()
+print(np.min(us))
+print(np.max(us))
+p = ax1.tripcolor(nodes[:,0],nodes[:,1],us,cmap='seismic',shading='gouraud',vmin=-0.1,vmax=0.1)
+plt.colorbar(p,ax=ax1)
+for s in smp:
+  ax1.plot(vert[s,0],vert[s,1],'k-')
 
-  time,data,var = extend_data(time,data,var)
+print(np.min(sigma))
+print(np.max(sigma))
+p = ax2.tripcolor(nodes[:,0],nodes[:,1],sigma,cmap='viridis',shading='gouraud',vmin=0.0,vmax=0.1)
+plt.colorbar(p,ax=ax2)
+for s in smp:
+  ax2.plot(vert[s,0],vert[s,1],'k-')
 
+# plot corr
+p = ax3.tripcolor(nodes[:,0],nodes[:,1],corr[0,:],
+                  cmap='seismic',shading='gouraud',vmin=-1.0,vmax=1.0)
+ax3.plot(nodes[0,0],nodes[0,1],'ko')
+plt.colorbar(p,ax=ax3)
+for s in smp:
+  ax3.plot(vert[s,0],vert[s,1],'k-')
 
+# plot corr
+p = ax4.tripcolor(nodes[:,0],nodes[:,1],corr[1,:],
+                  cmap='seismic',shading='gouraud',vmin=-1.0,vmax=1.0)
+ax4.plot(nodes[1,0],nodes[1,1],'ko')
+plt.colorbar(p,ax=ax4)
+for s in smp:
+  ax4.plot(vert[s,0],vert[s,1],'k-')
+
+ax1.grid()
+ax2.grid()
+ax3.grid()
+ax4.grid()
 plt.show()
-
+print(len(vert))
