e3feacf787d3acca09d8849c53a512252ff6233c
hinest <treverhines@gmail.com>
2016-11-03 10:51:56 -0400

    added more

---
+++ examples/figure2.py
@@ -0,0 +1,339 @@
+''' 
+In this script I illustrate the frequency content of the filtered 
+solution when idealized conditions are not met
+'''
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import rbf.filter
+import rbf.fd
+import rbf.halton
+from scipy.signal import periodogram
+import sympy
+from scipy.linalg import circulant
+from numpy.linalg import inv
+np.random.seed(2)
+
+def significant_eigenvectors(G):
+  ''' 
+  returns eigenvectors with a eigenvalue greater than 0.5
+  '''
+  val,vec = np.linalg.eig(G)
+  idx = np.argsort(val)[::-1]
+  val = val[idx]
+  vec = vec[:,idx]
+  idx = val > 0.5
+  vec = vec[:,idx]
+  return vec,val
+
+def spectral_diff_matrix(N,dt,diff):
+  ''' 
+  generates a periodic sinc differentation matrix. This is equivalent 
+  
+  Parameters
+  ----------
+    N : number of observations 
+    dt : sample spacing
+    diff : derivative order (max=2)
+
+  '''
+  scale = dt*N/(2*np.pi)
+  dt = 2*np.pi/N
+  t,h = sympy.symbols('t,h')
+  sinc = sympy.sin(sympy.pi*t/h)/((2*sympy.pi/h)*sympy.tan(t/2))
+  if diff == 0:
+    sinc_diff = sinc
+  else:
+    sinc_diff = sinc.diff(*(t,)*diff)
+
+  func = sympy.lambdify((t,h),sinc_diff,'numpy')
+  times = dt*np.arange(N)
+  val = func(times,dt)
+  if diff == 0:
+    val[0] = 1.0
+  elif diff == 1:
+    val[0] = 0.0
+  elif diff == 2:
+    val[0] = -(np.pi**2/(3*dt**2)) - 1.0/6.0
+
+  D = circulant(val)/scale**diff
+  return D
+
+fig,axs1 = plt.subplots(3,2,figsize=(7,5),
+                       gridspec_kw={'bottom':0.1,
+                                    'left':0.075,
+                                    'top':0.975,
+                                    'right':0.975,
+                                    'wspace':0.15,
+                                    'hspace':0.275})
+fig,axs2 = plt.subplots(3,2,figsize=(7,5),
+                        gridspec_kw={'bottom':0.1,
+                                     'left':0.075,
+                                     'top':0.975,
+                                     'right':0.975,
+                                     'wspace':0.15,
+                                     'hspace':0.275})
+
+axs = [[axs1[0][0],axs1[1][0],axs1[2][0]],
+       [axs1[0][1],axs1[1][1],axs1[2][1]],
+       [axs2[0][0],axs2[1][0],axs2[2][0]],
+       [axs2[0][1],axs2[1][1],axs2[2][1]]]
+
+
+axs[0][0].tick_params(labelsize=10)
+axs[0][1].tick_params(labelsize=10)
+axs[0][2].tick_params(labelsize=10)
+axs[1][0].tick_params(labelsize=10)
+axs[1][1].tick_params(labelsize=10)
+axs[1][2].tick_params(labelsize=10)
+axs[2][0].tick_params(labelsize=10)
+axs[2][1].tick_params(labelsize=10)
+axs[2][2].tick_params(labelsize=10)
+axs[3][0].tick_params(labelsize=10)
+axs[3][1].tick_params(labelsize=10)
+axs[3][2].tick_params(labelsize=10)
+axs[0][0].text(0.05,0.05,'A',transform=axs[0][0].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[0][1].text(0.05,0.05,'B',transform=axs[0][1].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[0][2].text(0.05,0.05,'C',transform=axs[0][2].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[1][0].text(0.05,0.05,'D',transform=axs[1][0].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[1][1].text(0.05,0.05,'E',transform=axs[1][1].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[1][2].text(0.05,0.05,'F',transform=axs[1][2].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[2][0].text(0.05,0.05,'A',transform=axs[2][0].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[2][1].text(0.05,0.05,'B',transform=axs[2][1].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[2][2].text(0.05,0.05,'C',transform=axs[2][2].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[3][0].text(0.05,0.05,'D',transform=axs[3][0].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[3][1].text(0.05,0.05,'E',transform=axs[3][1].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[3][2].text(0.05,0.05,'F',transform=axs[3][2].transAxes,
+        fontsize=12, fontweight='bold', va='bottom')
+axs[0][2].set_xlabel('eigenvalue number',fontsize=10,labelpad=-1)
+axs[1][2].set_xlabel('eigenvalue number',fontsize=10,labelpad=-1)
+axs[2][2].set_xlabel('eigenvalue number',fontsize=10,labelpad=-1)
+axs[3][2].set_xlabel('eigenvalue number',fontsize=10,labelpad=-1)
+axs[0][1].set_xlabel('x',fontsize=10,labelpad=-1)
+axs[1][1].set_xlabel('x',fontsize=10,labelpad=-1)
+axs[2][1].set_xlabel('x',fontsize=10,labelpad=-1)
+axs[3][1].set_xlabel('x',fontsize=10,labelpad=-1)
+axs[0][0].set_xlabel('x',fontsize=10,labelpad=-1)
+axs[1][0].set_xlabel('x',fontsize=10,labelpad=-1)
+axs[2][0].set_xlabel('x',fontsize=10,labelpad=-1)
+axs[3][0].set_xlabel('x',fontsize=10,labelpad=-1)
+
+col1_ylim = (-0.6,1.6)
+col2_ylim = (-0.25,0.25)
+col3_ylim = (3e-5,3e0)
+col3_xlim = (5e-1,1.7e2)
+gray1 = (0.6,0.6,0.6)
+gray2 = (0.6,0.6,0.6)
+
+N = 100
+w = 5.0 # cutoff frequency
+#####################################################################
+# idealized conditions:
+#   periodic spectral differentiation matrix
+#   constant uncertainty
+#   constant spacing
+x = np.linspace(0.0,1.0,N)
+s = 0.1*np.ones(N) # uncertainties
+t = np.sqrt(x) + 0.2*np.sin(4*np.pi*x) 
+u = t + np.random.normal(0.0,s)
+D = spectral_diff_matrix(N,x[1]-x[0],2)
+C = np.diag(1.0/s**2) # inverse covariance
+# generalized inverse
+b = np.sqrt(N/np.sum(1.0/s**2)) # mean uncertainty
+p = np.sqrt((2*np.pi*w)**4*b**2)
+G = inv(C + 1.0/p**2*D.T.dot(D)).dot(C)
+z = G.dot(u)
+y = np.sqrt(np.diag(inv(C + 1.0/p**2*D.T.dot(D))))
+# get eigenvalues and vectors
+vec,val = significant_eigenvectors(G)
+
+# Column 1
+ax = axs[0][0]
+ax.minorticks_on()
+ax.errorbar(x,u,s,fmt='.',capsize=0,label='observed',markersize=5,color=gray1,zorder=0)
+ax.plot(x,t,'r-',label='true signal',zorder=0.5)
+ax.plot(x,z,'b-',label='filtered',zorder=1)
+ax.fill_between(x,z-2*y,z+2*y,color='b',alpha=0.3,edgecolor='none',zorder=1)
+#ax.set_xlabel('x',fontsize=12)
+#ax.legend(loc=4,frameon=False,fontsize=12)
+ax.set_ylim(col1_ylim)
+#ax.grid()
+
+# Column 2
+ax = axs[0][1]
+ax.minorticks_on()
+ax.plot(x,vec,'-',color=gray2)
+ax.plot(x,vec[:,-1],'b-',lw=2)
+ax.set_ylim(col2_ylim)
+
+# Column 3
+ax = axs[0][2]
+ax.loglog(np.arange(1,N+1),val,'.',markersize=5,color='k')
+ax.loglog(np.arange(1,N+1)[val>0.5][-1],val[val>0.5][-1],'b.',mec='b',markersize=15)
+ax.set_ylim(col3_ylim)
+ax.set_xlim(col3_xlim)
+ax.grid()
+#ax.set_xlabel('x',fontsize=12)
+
+#####################################################################
+# conditions:
+#   constant uncertainty
+#   constant spacing
+x = np.linspace(0.0,1.0,N)
+s = 0.1*np.ones(N) # uncertainties
+t = np.sqrt(x) + 0.2*np.sin(4*np.pi*x) 
+u = t + np.random.normal(0.0,s)
+D = rbf.fd.weight_matrix(x[:,None],x[:,None],diffs=(2,)).toarray()
+C = np.diag(1.0/s**2) # inverse covariance
+# generalized inverse
+b = np.sqrt(N/np.sum(1.0/s**2)) # mean uncertainty
+p = np.sqrt((2*np.pi*w)**4*b**2)
+G = inv(C + 1.0/p**2*D.T.dot(D)).dot(C)
+z = G.dot(u)
+y = np.sqrt(np.diag(inv(C + 1.0/p**2*D.T.dot(D))))
+# get eigenvalues and vectors
+vec,val = significant_eigenvectors(G)
+
+# Column 1
+ax = axs[1][0]
+ax.minorticks_on()
+ax.errorbar(x,u,s,fmt='.',capsize=0,label='observed',markersize=5,color=gray1,zorder=0)
+ax.plot(x,t,'r-',label='true signal',zorder=0.5)
+ax.plot(x,z,'b-',label='filtered',zorder=1)
+ax.fill_between(x,z-2*y,z+2*y,color='b',alpha=0.3,edgecolor='none',zorder=1)
+#ax.set_xlabel('x',fontsize=12)
+#ax.legend(loc=4,frameon=False,fontsize=12)
+ax.set_ylim(col1_ylim)
+#ax.grid()
+
+# Column 2
+ax = axs[1][1]
+ax.minorticks_on()
+ax.plot(x,vec,'-',color=gray2)
+ax.plot(x,vec[:,-1],'b-',lw=2)
+#ax.set_xlabel('x',fontsize=12)
+ax.set_ylim(col2_ylim)
+
+# Column 3
+ax = axs[1][2]
+ax.loglog(np.arange(1,N+1),val,'.',markersize=5,color='k')
+ax.loglog(np.arange(1,N+1)[val>0.5][-1],val[val>0.5][-1],'b.',mec='b',markersize=15)
+ax.set_ylim(col3_ylim)
+ax.set_xlim(col3_xlim)
+ax.grid()
+
+#####################################################################
+# conditions:
+#   constant uncertainty
+#   constant spacing
+x = np.linspace(0.0,1.0,N)
+s = 0.1*np.ones(N) # uncertainties
+s[(x>0.2) & (x < 0.5)] = 0.3
+# XXXXXXXXXXXX
+s[(x>0.2) & (x < 0.5)] = np.inf
+# XXXXXXXXXXXX
+
+
+t = np.sqrt(x) + 0.2*np.sin(4*np.pi*x) 
+u = t + np.random.normal(0.0,s)
+D = rbf.fd.weight_matrix(x[:,None],x[:,None],diffs=(2,)).toarray()
+C = np.diag(1.0/s**2) # inverse covariance
+# generalized inverse
+b = np.sqrt(N/np.sum(1.0/s**2)) # mean uncertainty
+print('characteristic sigma %s' % b)
+p = np.sqrt((2*np.pi*w)**4*b**2)
+G = inv(C + 1.0/p**2*D.T.dot(D)).dot(C)
+z = G.dot(u)
+y = np.sqrt(np.diag(inv(C + 1.0/p**2*D.T.dot(D))))
+# get eigenvalues and vectors
+vec,val = significant_eigenvectors(G)
+print('min = %s' % np.min(val))
+print('max = %s' % np.max(val))
+
+# Column 1
+ax = axs[2][0]
+ax.minorticks_on()
+ax.errorbar(x,u,s,fmt='.',capsize=0,label='observed',markersize=5,color=gray1,zorder=0)
+ax.plot(x,t,'r-',label='true signal',zorder=0.5)
+ax.plot(x,z,'b-',label='filtered',zorder=1)
+ax.fill_between(x,z-2*y,z+2*y,color='b',alpha=0.3,edgecolor='none',zorder=1)
+#ax.set_xlabel('x',fontsize=12)
+#ax.legend(loc=4,frameon=False,fontsize=12)
+ax.set_ylim(col1_ylim)
+#ax.grid()
+
+# Column 2
+ax = axs[2][1]
+ax.minorticks_on()
+ax.plot(x,vec,'-',color=gray2)
+ax.plot(x,vec[:,-1],'b-',lw=2)
+ax.set_ylim(col2_ylim)
+#ax.set_xlabel('x',fontsize=12)
+
+# Column 3
+ax = axs[2][2]
+ax.loglog(np.arange(1,N+1),val,'.',markersize=5,color='k')
+ax.loglog(np.arange(1,N+1)[val>0.5][-1],val[val>0.5][-1],'b.',mec='b',markersize=15)
+ax.set_ylim(col3_ylim)
+ax.set_xlim(col3_xlim)
+ax.grid()
+
+#####################################################################
+# conditions:
+#   constant uncertainty
+#   constant spacing
+x = np.linspace(0.05,1.0,N)**2
+s = 0.1*np.ones(N) # uncertainties
+t = np.sqrt(x) + 0.2*np.sin(4*np.pi*x) 
+u = t + np.random.normal(0.0,s)
+D = rbf.fd.weight_matrix(x[:,None],x[:,None],diffs=(2,)).toarray()
+C = np.diag(1.0/s**2) # inverse covariance
+# generalized inverse
+b = np.sqrt(N/np.sum(1.0/s**2)) # mean uncertainty
+print('characteristic sigma %s' % b)
+p = np.sqrt((2*np.pi*w)**4*b**2)
+G = inv(C + 1.0/p**2*D.T.dot(D)).dot(C)
+z = G.dot(u)
+y = np.sqrt(np.diag(inv(C + 1.0/p**2*D.T.dot(D))))
+# get eigenvalues and vectors
+vec,val = significant_eigenvectors(G)
+
+# Column 1
+ax = axs[3][0]
+ax.minorticks_on()
+ax.errorbar(x,u,s,fmt='.',capsize=0,label='observed',markersize=5,color=gray1,zorder=0)
+ax.plot(x,t,'r-',label='true signal',zorder=0.5)
+ax.plot(x,z,'b-',label='filtered',zorder=1)
+ax.fill_between(x,z-2*y,z+2*y,color='b',alpha=0.3,edgecolor='none',zorder=1)
+#ax.legend(loc=4,frameon=False,fontsize=12)
+ax.set_ylim(col1_ylim)
+#ax.grid()
+
+# Column 2
+ax = axs[3][1]
+ax.minorticks_on()
+ax.plot(x,vec,'-',color=gray2)
+ax.plot(x,vec[:,-1],'b-',lw=2)
+ax.set_ylim(col2_ylim)
+
+# Column 3
+ax = axs[3][2]
+ax.loglog(np.arange(1,N+1),val,'.',markersize=5,color='k')
+ax.loglog(np.arange(1,N+1)[val>0.5][-1],val[val>0.5][-1],'b.',mec='b',markersize=15)
+ax.set_ylim(col3_ylim)
+ax.set_xlim(col3_xlim)
+ax.grid()
+
+
+plt.show()
